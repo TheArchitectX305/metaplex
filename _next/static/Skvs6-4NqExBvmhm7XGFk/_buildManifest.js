71f83911b4197cdb0f2f7c8a3ba8e9e37d73cf62
TheArchitectX <95813532+TheArchitectX305@users.noreply.github.com>
2022-02-03 00:59:18 -0500

    Updates

---
+++ _next/static/Skvs6-4NqExBvmhm7XGFk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-4871ed6ee0c3650f6e1b.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
